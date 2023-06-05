c837424949ff473d0bbdf1fd681b5c0d76aab010
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-06-05 18:20:02 +0200

    hw6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from '@reduxjs/toolkit';
 import { AppStyle } from './App.styled';
@@ -11,18 +11,6 @@ const App = () => {
   const dispatch = useDispatch();
  const contacts = useSelector(state => state.contacts.contacts);
 const filter = useSelector(state => state.contacts.filter);
-
-
-  useEffect(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      dispatch(addContact(JSON.parse(savedContacts)));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   const handleAddContact = (name, number) => {
     const existingContact = contacts.find(
@@ -51,10 +39,10 @@ const filter = useSelector(state => state.contacts.filter);
     dispatch(updateFilter(e.target.value));
   };
 
- const filteredContacts = contacts.filter((contact) =>
+ const filteredContacts = contacts?.filter((contact) =>
   contact.name.toLowerCase().includes(filter.toLowerCase())
 );
-console.log(contacts);
+
   return (
     <AppStyle.AppContainer>
       <AppStyle.AppTitle>Phonebook</AppStyle.AppTitle>
